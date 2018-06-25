df9f6a3e31cf460e56545098b16275ccc76eaef4
rbardaji <rbardaji@gmail.com>
2018-06-25 11:54:07 +0200

    Save data into SD

--- KdUINO_Feather/src/main.cpp
+++ KdUINO_Feather/src/main.cpp
@@ -6,8 +6,8 @@
 #include "Adafruit_TCS34725.h"
 
 // Settings
-int initial_wait = 5;       // Time to wait before start the loop (in seconds)
-int measures = 1;           // Number of measurements to do[1, 59]
+int initial_wait = 60;       // Time to wait before start the loop (in seconds)
+int measures = 5;           // Number of measurements to do[1, 59]
 int period = 1;             // Sampling period (in minutes) [1, 60]
 float depth = 0.3;          // Absolute depth of the device [0.1, 30] (in meters)
 float lat = 0;              // Latitude
@@ -21,7 +21,8 @@ String email = "";          // Email of the curator
 String sensors = "TCS34725";// List with name of used sensors "Sensor 1, ..., Sensor n"
 String description = "Test prototype";
 String place = "lab ICM";   // Text with place of deployment
-String units = "";          // Units of the measurements "Unit 1, ..., Unit n"
+                            // Units of the measurements "Unit 1, ..., Unit n"
+String units = "counts, counts, counts, counts, lux, degree_celsius, percent";
 
 // Contants
 #define BAUDRATE 9600
@@ -36,12 +37,17 @@ const int chipSelect_SD = 15;
 Adafruit_TCS34725 tcs = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_700MS, TCS34725_GAIN_1X);
 uint16_t r, g, b, c, colorTemp, lux;
 float battery_level;
+DateTime now;
 
 // Function declaration
 void measure_TCS34725();
 void measure_battery();
 void serial_data();
+void serial_metadata();
+void serial_header();
 void save_data();
+void save_metadata();
+void save_header();
 
 void setup () {
     // CONFIGURATION
@@ -62,6 +68,7 @@ void setup () {
         digitalWrite(REDLED, LOW);
         while (1);
     }
+    rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
     Serial.println(" Done.");
     
     // SD
@@ -89,12 +96,20 @@ void setup () {
     // Initial wait
     delay(initial_wait*1000);
     
+    // Write metadata and header into file.txt
+    save_metadata();
+    save_header();
+
+    // Send metadata and header through serial communication
+    serial_metadata();
+    serial_header();
+
     digitalWrite(BLUELED, HIGH);
 }
 
 void loop () {
     // Read time
-    DateTime now = rtc.now();
+    now = rtc.now();
     
     // Check if it is time to measure
     if (now.minute() % period == 0){
@@ -110,6 +125,10 @@ void loop () {
     }
     delay(500);
 }
+
+////////////////////////////////////////////////////////////
+//////////////////// MEASUREMENTS //////////////////////////
+////////////////////////////////////////////////////////////
 
 void measure_TCS34725(){
     tcs.getRawData(&r, &g, &b, &c);
@@ -129,7 +148,24 @@ void measure_battery(){
     battery_level = map(battery_level, 580, 774, 0, 100);
 }
 
+////////////////////////////////////////////////////////////
+//////////////////// SERIAL COMMUNICATION //////////////////
+////////////////////////////////////////////////////////////
+
 void serial_data(){
+    /*It sends the data throught the serial  communication*/
+    Serial.print(now.year(), DEC);
+    Serial.print("/");
+    Serial.print(now.month(), DEC);
+    Serial.print("/");
+    Serial.print(now.day(), DEC);
+    Serial.print(" ");
+    Serial.print(now.hour(), DEC);
+    Serial.print(":");
+    Serial.print(now.minute(), DEC);
+    Serial.print(":");
+    Serial.print(now.second(), DEC);
+    Serial.print(" ");
     Serial.print(r, DEC);
     Serial.print(" ");
     Serial.print(g, DEC);
@@ -146,6 +182,156 @@ void serial_data(){
     Serial.println("");
 }
 
+void serial_metadata(){
+    /*It sends the metadata info throught the serial  communication*/
+    Serial.println("METADATA");
+    Serial.print("initial_wait: ");
+    Serial.println(initial_wait, DEC);
+    Serial.print("measures: ");
+    Serial.println(measures, DEC);
+    Serial.print("period: ");
+    Serial.println(period, DEC);
+    Serial.print("depth: ");
+    Serial.println(depth);
+    Serial.print("lat: ");
+    Serial.println(lat);
+    Serial.print("lon: ");
+    Serial.println(lon);
+    Serial.print("timestamp: ");
+    Serial.println(timestamp);
+    Serial.print("sample_counter: ");
+    Serial.println(sample_counter);
+    Serial.print("name: ");
+    Serial.println(name);
+    Serial.print("maker: ");
+    Serial.println(maker);
+    Serial.print("curator: ");
+    Serial.println(curator);
+    Serial.print("email: ");
+    Serial.println(email);
+    Serial.print("sensors: ");
+    Serial.println(sensors);
+    Serial.print("description: ");
+    Serial.println(description);
+    Serial.print("place: ");
+    Serial.println(place);
+    Serial.print("units: ");
+    Serial.println(units);
+}
+
+void serial_header(){
+    /*It sends the header info of the datathrought the serial
+    communication*/
+    Serial.println("DATA");
+    Serial.println("DATE HOUR RED GREEN BLUE CLEAR LUX COLOR_TEMP BATTERY");
+}
+
+////////////////////////////////////////////////////////////
+//////////////////// SD MANAGEMENT /////////////////////////
+////////////////////////////////////////////////////////////
+
 void save_data(){
-    Serial.print("Estoy guardanto");
+    // open the file. note that only one file can be open at a time,
+    // so you have to close this one before opening another.
+    File data_file = SD.open("data.txt", FILE_WRITE);
+    if (! data_file) {
+        Serial.println("error opening data.txt");
+        digitalWrite(REDLED, LOW);
+        // Wait forever since we cant write data
+        while (1) ;
+    }
+    // Save data
+    data_file.print(now.year(), DEC);
+    data_file.print("/");
+    data_file.print(now.month(), DEC);
+    data_file.print("/");
+    data_file.print(now.day(), DEC);
+    data_file.print(" ");
+    data_file.print(now.hour(), DEC);
+    data_file.print(":");
+    data_file.print(now.minute(), DEC);
+    data_file.print(":");
+    data_file.print(now.second(), DEC);
+    data_file.print(" ");
+    data_file.print(r, DEC);
+    data_file.print(" ");
+    data_file.print(g, DEC);
+    data_file.print(" ");
+    data_file.print(b, DEC);
+    data_file.print(" ");
+    data_file.print(c, DEC);
+    data_file.print(" ");
+    data_file.print(lux);
+    data_file.print(" ");
+    data_file.print(colorTemp);
+    data_file.print(" ");
+    data_file.print(battery_level);
+    data_file.println("");
+    // Close dataFile
+    data_file.close();
+}
+
+void save_metadata(){
+    // open the file. note that only one file can be open at a time,
+    // so you have to close this one before opening another.
+    File data_file = SD.open("data.txt", FILE_WRITE);
+    if (! data_file) {
+        Serial.println("error opening data.txt");
+        digitalWrite(REDLED, LOW);
+        // Wait forever since we cant write data
+        while (1) ;
+    }
+    // Save metadata
+    data_file.println("METADATA");
+    data_file.print("initial_wait: ");
+    data_file.println(initial_wait, DEC);
+    data_file.print("measures: ");
+    data_file.println(measures, DEC);
+    data_file.print("period: ");
+    data_file.println(period, DEC);
+    data_file.print("depth: ");
+    data_file.println(depth);
+    data_file.print("lat: ");
+    data_file.println(lat);
+    data_file.print("lon: ");
+    data_file.println(lon);
+    data_file.print("timestamp: ");
+    data_file.println(timestamp);
+    data_file.print("sample_counter: ");
+    data_file.println(sample_counter);
+    data_file.print("name: ");
+    data_file.println(name);
+    data_file.print("maker: ");
+    data_file.println(maker);
+    data_file.print("curator: ");
+    data_file.println(curator);
+    data_file.print("email: ");
+    data_file.println(email);
+    data_file.print("sensors: ");
+    data_file.println(sensors);
+    data_file.print("description: ");
+    data_file.println(description);
+    data_file.print("place: ");
+    data_file.println(place);
+    data_file.print("units: ");
+    data_file.println(units);
+    // Close dataFile
+    data_file.close();
+}
+
+void save_header(){
+    // open the file. note that only one file can be open at a time,
+    // so you have to close this one before opening another.
+    File data_file = SD.open("data.txt", FILE_WRITE);
+    if (! data_file) {
+        Serial.println("error opening data.txt");
+        digitalWrite(REDLED, LOW);
+        // Wait forever since we cant write data
+        while (1) ;
+    }
+    // Save header
+    data_file.println("DATA");
+    data_file.println("DATE HOUR RED GREEN BLUE CLEAR LUX COLOR_TEMP BATTERY");
+    // Close dataFile
+    data_file.close();
 }
